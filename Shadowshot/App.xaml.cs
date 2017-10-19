8f27fc928fad0169df7374ca501f32e4d028b96d
Victorique Ko <victoriqueko@users.noreply.github.com>
2017-10-19 20:42:41 +0800

    Rewrite with WPF

---
+++ Shadowshot/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shadowshot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Locator.CurrentMutable.RegisterConstant(FindResource("NotifyIcon"), typeof(TaskbarIcon));
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            Locator.CurrentMutable.GetService<TaskbarIcon>().Dispose();
+        }
+    }
+}
---
+++ Shadowshot/ViewModels/NotifyIcon.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI;
+using System.Reactive;
+using System.Windows;
+
+namespace Shadowshot.ViewModels
+{
+    class NotifyIcon : ReactiveObject
+    {
+        public NotifyIcon()
+        {
+            ExitCommand = ReactiveCommand.Create(() => Application.Current.Shutdown());
+        }
+
+        public ReactiveCommand<Unit, Unit> ExitCommand { get; }
+    }
+}
