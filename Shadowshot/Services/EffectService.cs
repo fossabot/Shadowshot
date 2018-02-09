f862083ab6726975c48d070db7dd468ec8857835
Victorique Ko <victoriqueko@users.noreply.github.com>
2018-02-10 01:34:10 +0800

    Use StackBlur

--- Shadowshot/Services/EffectService.cs
+++ Shadowshot/Services/EffectService.cs
@@ -15,9 +15,8 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Linq;
-using System.Runtime.InteropServices;
+using StackBlur.Extensions;
 
 namespace Shadowshot.Services
 {
@@ -89,180 +88,10 @@ namespace Shadowshot.Services
                         cfg.DistanceY > 0 ? cfg.Radius + cfg.DistanceY : cfg.Radius,
                         bitmap.Width, bitmap.Height));
             }
-
-            GaussianBlur(shadow, cfg.Radius);
+            
+            shadow.StackBlur(cfg.Radius);
 
             return shadow;
-        }
-
-        private static void GaussianBlur(Bitmap bitmap, int radius)
-        {
-            var rect = new Rectangle(Point.Empty, bitmap.Size);
-            var data = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
-            var bytes = new int[Math.Abs(data.Stride) * data.Height / 4];
-            Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
-
-            var alpha = new int[data.Width * data.Height];
-            var red = new int[data.Width * data.Height];
-            var green = new int[data.Width * data.Height];
-            var blue = new int[data.Width * data.Height];
-            for (var i = 0; i < data.Width; i++)
-            for (var j = 0; j < data.Height; j++)
-            {
-                alpha[i + j * data.Width] = (int) ((bytes[i + j * Math.Abs(data.Stride) / 4] & 0xff000000) >> 24);
-                red[i + j * data.Width] = (bytes[i + j * Math.Abs(data.Stride) / 4] & 0x00ff0000) >> 16;
-                green[i + j * data.Width] = (bytes[i + j * Math.Abs(data.Stride) / 4] & 0x0000ff00) >> 8;
-                blue[i + j * data.Width] = bytes[i + j * Math.Abs(data.Stride) / 4] & 0x000000ff;
-            }
-
-            alpha = GaussianBlurChannel(alpha, data.Width, data.Height, radius);
-            red = GaussianBlurChannel(red, data.Width, data.Height, radius);
-            green = GaussianBlurChannel(green, data.Width, data.Height, radius);
-            blue = GaussianBlurChannel(blue, data.Width, data.Height, radius);
-
-            for (var i = 0; i < data.Width; i++)
-            for (var j = 0; j < data.Height; j++)
-            {
-                if (alpha[i + j * data.Width] < 0)
-                    alpha[i + j * data.Width] = 0;
-                else if (alpha[i + j * data.Width] > 255)
-                    alpha[i + j * data.Width] = 255;
-
-                if (red[i + j * data.Width] < 0)
-                    red[i + j * data.Width] = 0;
-                else if (red[i + j * data.Width] > 255)
-                    red[i + j * data.Width] = 255;
-
-                if (green[i + j * data.Width] < 0)
-                    green[i + j * data.Width] = 0;
-                else if (green[i + j * data.Width] > 255)
-                    green[i + j * data.Width] = 255;
-
-                if (blue[i + j * data.Width] < 0)
-                    blue[i + j * data.Width] = 0;
-                else if (blue[i + j * data.Width] > 255)
-                    blue[i + j * data.Width] = 255;
-
-                bytes[i + j * Math.Abs(data.Stride) / 4] =
-                    (alpha[i + j * data.Width] << 24) |
-                    (red[i + j * data.Width] << 16) |
-                    (green[i + j * data.Width] << 8) |
-                    blue[i + j * data.Width];
-            }
-
-            Marshal.Copy(bytes, 0, data.Scan0, bytes.Length);
-            bitmap.UnlockBits(data);
-        }
-
-        private static int[] GaussianBlurChannel(IReadOnlyList<int> channel, int width, int height, int radius)
-        {
-            const int passes = 3;
-            var widthList = BoxesForGauss(radius, passes);
-            var result = BoxBlurChannel(channel, width, height, (widthList[0] - 1) / 2);
-            for (var i = 1; i < passes; i++)
-                result = BoxBlurChannel(result, width, height, (widthList[i] - 1) / 2);
-            return result;
-        }
-
-        private static int[] BoxesForGauss(int radius, int n)
-        {
-            var sigma = (radius - 1) * 0.3 + 0.8;
-            var widthIdeal = Math.Sqrt(12 * sigma * sigma / n + 1);
-            var widthLess = (int) widthIdeal % 2 != 0 ? (int) widthIdeal - 1 : (int) widthIdeal;
-            var widthGreater = widthLess + 2;
-            var m = (12 * sigma * sigma - n * widthLess * widthLess - 4 * n * widthLess - 3 * n) / (-4 * widthLess - 4);
-
-            var widthList = new int[n];
-            for (var i = 0; i < n; i++)
-                widthList[i] = i < m ? widthLess : widthGreater;
-
-            return widthList;
-        }
-
-        private static int[] BoxBlurChannel(IReadOnlyList<int> channel, int width, int height, int radius)
-        {
-            var result = BoxBlurChannelH(channel, width, height, radius);
-            result = BoxBlurChannelT(result, width, height, radius);
-            return result;
-        }
-
-        private static int[] BoxBlurChannelH(IReadOnlyList<int> channel, int width, int height, int radius)
-        {
-            var result = new int[channel.Count];
-            var iarr = 1.0 / (radius + radius + 1);
-
-            for (var i = 0; i < height; i++)
-            {
-                var ti = i * width;
-                var li = ti;
-                var ri = ti + radius;
-                var fv = channel[ti];
-                var lv = channel[ti + width - 1];
-                var val = (radius + 1) * fv;
-
-                for (var j = 0; j < radius; j++)
-                    val += channel[ti + j];
-                for (var j = 0; j <= radius; j++)
-                {
-                    val += channel[ri++] - fv;
-                    result[ti++] = (int) Math.Round(val * iarr);
-                }
-                for (var j = radius + 1; j < width - radius; j++)
-                {
-                    val += channel[ri++] - result[li++];
-                    result[ti++] = (int) Math.Round(val * iarr);
-                }
-                for (var j = width - radius; j < width; j++)
-                {
-                    val += lv - channel[li++];
-                    result[ti++] = (int) Math.Round(val * iarr);
-                }
-            }
-
-            return result;
-        }
-
-        private static int[] BoxBlurChannelT(IReadOnlyList<int> channel, int width, int height, int radius)
-        {
-            var result = new int[channel.Count];
-            var iarr = 1.0 / (radius + radius + 1);
-
-            for (var i = 0; i < width; i++)
-            {
-                var ti = i;
-                var li = ti;
-                var ri = ti + radius * width;
-                var fv = channel[ti];
-                var lv = channel[ti + width * (height - 1)];
-                var val = (radius + 1) * fv;
-
-                for (var j = 0; j < radius; j++)
-                    val += channel[ti + j * width];
-                for (var j = 0; j <= radius; j++)
-                {
-                    val += channel[ri] - fv;
-                    result[ti] = (int) Math.Round(val * iarr);
-                    ri += width;
-                    ti += width;
-                }
-                for (var j = radius + 1; j < height - radius; j++)
-                {
-                    val += channel[ri] - channel[li];
-                    result[ti] = (int) Math.Round(val * iarr);
-                    li += width;
-                    ri += width;
-                    ti += width;
-                }
-                for (var j = height - radius; j < height; j++)
-                {
-                    val += lv - channel[li];
-                    result[ti] = (int) Math.Round(val * iarr);
-                    li += width;
-                    ti += width;
-                }
-            }
-
-            return result;
         }
 
         private struct ShadowConfig
