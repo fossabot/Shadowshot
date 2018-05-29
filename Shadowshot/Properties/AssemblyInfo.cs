27393968623e8202318bd3443949a89e7f8297c2
Victorique Ko <victoriqueko@users.noreply.github.com>
2018-05-29 06:15:19 -0700

    Bump version

--- Shadowshot/Properties/AssemblyInfo.cs
+++ Shadowshot/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Shadowshot")]
 [assembly: AssemblyDescription("Shadowshot")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kagami Studio")]
+[assembly: AssemblyCompany("Victorique Ko")]
 [assembly: AssemblyProduct("Shadowshot")]
-[assembly: AssemblyCopyright("Copyright © Kagami Studio 2017-2018")]
+[assembly: AssemblyCopyright("Copyright © Victorique Ko 2017-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.4.0")]
-[assembly: AssemblyFileVersion("0.2.4.0")]
+[assembly: AssemblyVersion("0.2.5.0")]
+[assembly: AssemblyFileVersion("0.2.5.0")]
